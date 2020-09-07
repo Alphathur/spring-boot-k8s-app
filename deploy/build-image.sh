5652ef81e63ba06a924ecab455a9066e80845870
Alphathur <1274221961@qq.com>
2020-09-07 16:50:37 +0800

    bugfix

--- deploy/build-image.sh
+++ deploy/build-image.sh
@@ -5,13 +5,16 @@ if [ $# -eq 1 ]; then
     tag=$1
 fi
 image=localhost:5000/spring-boot-k8s-app:$tag
+image-name=spring-boot-k8s-app:$tag
+
 docker build -t $image .
 docker push $image
 echo "$image"
 
 cd deploy
-sed -i s#192.168.6.128:5000/spring-boot-k8s-app:[0-9]*-[0-9]*#$image
-grep "192.168.6.128:5000/spring-boot-k8s-app" spring-boot-deployment.yaml
+sed -i s#spring-boot-k8s-app:[0-9]*-[0-9]*#$image-name
+#sed -i s#192.168.6.128:5000/spring-boot-k8s-app:[0-9]*-[0-9]*#$image
+#grep "192.168.6.128:5000/spring-boot-k8s-app" spring-boot-deployment.yaml
 
 cat spring-boot-deployment.yaml
 # kubectl apply -f spring-boot-deployment.yaml
